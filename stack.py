18fda9ece3b3b298770125f419bc4e9739a7711e
Faeze <amini.faeze300@gmail.com>
2016-05-09 14:29:15 +0430

    Add stack class

---
+++ stack.py
@@ -0,0 +1,23 @@
+#!usr/bin/env python3
+
+class Stack(object):
+    def __init__(self,lst=[]):
+        self.lst = lst
+
+    def top(self):
+        if len(self.lst)>0:
+            return self.lst[len(self.lst)-1]
+        else:
+            return None
+
+    def pop(self):
+        if len(self.lst)>0:
+            return self.lst.pop()
+        else:
+            return None
+
+    def length(self):
+        return len(self.lst)
+
+    def push(self,x):
+        return self.lst.append(x)
